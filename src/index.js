07c5d0b4a441e12e88fd5f9519f425a660dcc647
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-10-06 09:27:50 +0000

    1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+const socketio = require("socket.io")
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 const port = 8080;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
+
+let message = "Welcome Socket";
+
+io.on("connection", (socket) => {
+    console.log("Nwe WebSocket Connection");
+    socket.emit("userConnect", message);
+})
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
