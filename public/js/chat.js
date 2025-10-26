5d39b74e939afcb06353e1d946d54f25d3865962
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-10-26 14:56:50 +0000

    1

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,16 +4,63 @@ const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm.querySelector("input");
 const $messageFormButton = $messageForm.querySelector("button");
 const $messages = document.querySelector("#messages");
+const $fileTransButton = document.querySelector("#file-transfer");
+const $fileInput = document.querySelector("#file-input");
 
 const messageTemplate = document.querySelector("#message-template").innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
+const fileUploadTemplate = document.querySelector("#file-upload").innerHTML;
 
-socket.on("message", (message) => {
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+});
+
+const autoscroll = () => {
+    const element = $messages.lastElementChildl;
+    element.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+    });
+};
+
+socket.on("userConnect", (message) => {
     const html = Mustache.render(messageTemplate, {
         text: message.text,
         createdAt: moment(message.createdAt).format("h:mm a"),
     });
     $messages.insertAdjacentHTML("beforeend", html);
 });
+
+$messageForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    $messageFormButton.setAttribute("disabled", "disabled");
+    const message = e.target.elements.message.value;
+
+    socket.emit("sendMessage", message, (error) => {
+        $messageFormButton.removeAttribute("disabled");
+        $messageFormInput.value = "";
+        $messageFormInput.focus();
+
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log("Message Delivered");
+    });
+
+});
+
+socket.emit("join", {username, room}, (error) => {
+    if (error) {
+        alert(error);
+        location.href = "/";
+    }
+
+});
+
+
 
 document.querySelector("#message-form").addEventListener("submit", (e) => {
     e.preventDefault();
